950d91261ba7a326a08af4ac4f01575091ef1b89
space <enaenen@naver.com>
2020-10-21 19:26:17 +0900

    Entitiy Create

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.graalvm.compiler.core.common.type.ArithmeticOpTable;
+
+import javax.persistence.*;
+
+@Entity
+@Getter@Setter
+public class Delivery {
+    @Id@GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; // REDAY, COMP
+}
