5aaeda50c00f16a96e2dfb987da0352966648d54
space <enaenen@naver.com>
2020-10-21 19:30:22 +0900

    값타입 변경

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
@@ -10,4 +11,13 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+    protected Address() {
+        //JPA 스펙상 필요- JPA 구현 라이브러리가 객체 생성시 리플렉션 같은 기술들을 사용할 수 있도록 지원
+    }
 }
