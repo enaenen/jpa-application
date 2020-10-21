fad78c6119359eba16e63afa9ace74d1a42329fd
space <enaenen@naver.com>
2020-10-22 01:23:26 +0900

    MemberRepository MemberService validation

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     */
+    @Transactional(readOnly = false)
+    public Long join(Member member){
+        validateDuplicateMember(member);    //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+//Unique 제약조건을 걸어서 한번 더 확인한다. 이 메소드 만으로는 멀티 쓰레드 환경에서 안전하지 않다.
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getUsername());
+        if(!findMembers.isEmpty())
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+    }
+
+    //회원 전체 조회
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
