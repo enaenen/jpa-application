8120dd54a501da922242198cb11b890b34c5b43f
Space <enaenen@naver.com>
2021-01-11 23:33:09 +0900

    API V2 회원등록

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +13,6 @@ public class Member {
     @Id @GeneratedValue
     @Column(name="member_id")
     private Long id;
-
     private String name;
 
     @Embedded
