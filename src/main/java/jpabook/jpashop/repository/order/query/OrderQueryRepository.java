af08a457c3b27fcdae7b089858f11353a60aa624
Space <enaenen@naver.com>
2021-01-14 21:50:57 +0900

    V5 V4의 N+1 해결 및 쿼리 2번으로 해결 쿼리에서 IN 절 사용

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -16,11 +18,41 @@ public class OrderQueryRepository {
     public List<OrderQueryDto> findOrderQueryDtos() {
         List<OrderQueryDto> result = findOrders();
         result.forEach(o->{
-            //OrderItems 를 가져와서 채워준다
+            //OrderItems 를 루프돌면서 가져와 채워준다
             List<OrderItemQueryDto> orderItems = findOrderItems(o.getOrderId());
             o.setOrderItems(orderItems);
         });
         return result;
+    }
+
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();
+        //OrderId의 리스트
+        List<Long> orderIds = toOrderIds(result);
+        //쿼리는 한번
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = findOrderItemMap(orderIds);
+        //MAP에서 찾아서 Order에 셋팅
+        result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
+        return result;
+    }
+
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+                "SELECT new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id,oi.item.name,oi.orderPrice,oi.count)" +
+                        " FROM OrderItem oi" +
+                        " JOIN oi.item i" +
+                        " WHERE oi.order.id IN :orderIds", OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+        //MAP으로 변경- 메모리에 로딩 후 forEach로 OrderQueryDto에 셋팅
+        return orderItems.stream()
+                .collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
+    }
+
+    private List<Long> toOrderIds(List<OrderQueryDto> result) {
+        return result.stream()
+                    .map(o -> o.getOrderId())
+                    .collect(Collectors.toList());
     }
 
     private List<OrderItemQueryDto> findOrderItems(Long orderId) {
@@ -41,4 +73,6 @@ public class OrderQueryRepository {
                         " JOIN o.delivery d", OrderQueryDto.class)
                 .getResultList();
     }
+
+
 }
