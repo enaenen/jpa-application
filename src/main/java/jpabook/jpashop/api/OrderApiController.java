e4b9de9d617d9e10eecefd020aa7da6615eaa99d
Space <enaenen@naver.com>
2021-01-15 08:45:14 +0900

    V6 flat 조회

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
+import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Getter;
@@ -18,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -38,47 +42,67 @@ public class OrderApiController {
         }
         return all;
     }
+
     @GetMapping("/api/v2/orders")
-    public List<OrderDto>ordersV2(){
+    public List<OrderDto> ordersV2() {
         List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
         return result;
     }
+
     @GetMapping("/api/v3/orders")
-    public List<OrderDto> ordersV3(){
-       List<Order> orders =  orderRepository.findWithItem();
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findWithItem();
 //        for (Order order : orders) {
 //            System.out.println("order ref  = "+order+" id = " + order.getId());
 //        }
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
         return result;
     }
+
     @GetMapping("/api/v3.1/orders")
     public List<OrderDto> ordersV3_page(
             @RequestParam(value = "offset", defaultValue = "0") int offset,
-            @RequestParam(value = "limit", defaultValue = "100") int limit)
-    {
-        List<Order> orders =  orderRepository.findAllWithMemberDelivery(offset, limit);
+            @RequestParam(value = "limit", defaultValue = "100") int limit) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
 
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
         return result;
     }
+
     @GetMapping("/api/v4/orders")
-    public List<OrderQueryDto> ordersV4(){
-       return orderQueryRepository.findOrderQueryDtos();
+    public List<OrderQueryDto> ordersV4() {
+        return orderQueryRepository.findOrderQueryDtos();
     }
+
     @GetMapping("/api/v5/orders")
-    public List<OrderQueryDto> ordersV5(){
+    public List<OrderQueryDto> ordersV5() {
         return orderQueryRepository.findAllByDto_optimization();
     }
+
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> ordersV6() {
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+        return flats.stream()
+                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(),
+                                o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o -> new OrderItemQueryDto(o.getOrderId(),
+                                o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+                )).entrySet().stream()
+                .map(e -> new OrderQueryDto(e.getKey().getOrderId(),
+                        e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(),
+                        e.getKey().getAddress(), e.getValue()))
+                .collect(toList());
+    }
+
     @Getter
-    static class OrderDto{
+    static class OrderDto {
         private Long orderId;
         private String name;
         private LocalDateTime orderDate;
@@ -87,27 +111,28 @@ public class OrderApiController {
         private List<OrderItemDto> orderItems;
 
         public OrderDto(Order order) {
-            orderId=order.getId();
-            name=order.getMember().getName();
-            orderDate=order.getOrderDate();
-            orderStatus=order.getStatus();
-            address=order.getDelivery().getAddress();
-            orderItems=order.getOrderItems()
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems()
                     .stream()
                     .map(orderItem -> new OrderItemDto(orderItem))
-                    .collect(Collectors.toList());
+                    .collect(toList());
         }
     }
+
     @Getter
-    static class OrderItemDto{
+    static class OrderItemDto {
         private String itemName; //상품 명
         private int orderPrice; //주문 가격
         private int count;      //주문 수량
 
         public OrderItemDto(OrderItem orderItem) {
             itemName = orderItem.getItem().getName();
-            orderPrice=orderItem.getOrderPrice();
-            count=orderItem.getCount();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
         }
     }
 }
--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.repository.order.query;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
  * 같은패키지에 만들었다.
  */
 @Data
+@EqualsAndHashCode(of = "orderId")
 public class OrderQueryDto {
     private Long orderId;
     private String name;
@@ -27,5 +29,14 @@ public class OrderQueryDto {
         this.orderDate = orderDate;
         this.orderStatus = orderStatus;
         this.address = address;
+    }
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
     }
 }
