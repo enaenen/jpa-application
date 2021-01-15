a174c7d1fab80bcb33eb8b7449eec757be0dde67
Space <enaenen@naver.com>
2021-01-15 09:39:16 +0900

    OSIV 를 off 했을경우 query 패키지 만들기

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -10,6 +10,8 @@ import jpabook.jpashop.repository.order.query.OrderFlatDto;
 import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
+import jpabook.jpashop.service.query.OrderDto;
+import jpabook.jpashop.service.query.OrderQueryService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.jaxb.SpringDataJaxb;
@@ -51,17 +53,15 @@ public class OrderApiController {
                 .collect(toList());
         return result;
     }
-
+    private final OrderQueryService orderQueryService;
     @GetMapping("/api/v3/orders")
-    public List<OrderDto> ordersV3() {
-        List<Order> orders = orderRepository.findWithItem();
-//        for (Order order : orders) {
-//            System.out.println("order ref  = "+order+" id = " + order.getId());
-//        }
-        List<OrderDto> result = orders.stream()
-                .map(o -> new OrderDto(o))
-                .collect(toList());
-        return result;
+    public List<jpabook.jpashop.service.query.OrderDto> ordersV3() {
+//        List<Order> orders = orderRepository.findWithItem();
+//        List<OrderDto> result = orders.stream()
+//                .map(o -> new OrderDto(o))
+//                .collect(toList());
+//        return result;
+        return orderQueryService.ordersV3();
     }
 
     @GetMapping("/api/v3.1/orders")
--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,12 +1,11 @@
 package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.MemberRepositoryOld;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,14 +19,15 @@ import static org.junit.Assert.*;
 public class MemberServiceTest {
 
     @Autowired MemberService memberService;
-    @Autowired MemberRepository memberRepository;
+    @Autowired
+    MemberRepositoryOld memberRepositoryOld;
     @Autowired EntityManager em;
 
     @Test
     public void 회원가입() throws Exception {
         //given
         Member member = new Member();
-        member.setUsername("chae");
+        member.setName("chae");
 
         //when
         Long savedId = memberService.join(member);
@@ -35,16 +35,16 @@ public class MemberServiceTest {
         //then
         //DB에 반영(Rollback 되기때문에 flush가 없을경우 JPA가 DB에 데이터를 넣을 필요성을 느끼지 못하여 insert 문이 보이지 않는다.)
         em.flush();
-        assertEquals(member, memberRepository.findOne(savedId));
+        assertEquals(member, memberRepositoryOld.findOne(savedId));
     }
     @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception {
         //given
         Member member1 = new Member();
-        member1.setUsername("kim1");
+        member1.setName("kim1");
 
         Member member2 = new Member();
-        member2.setUsername("kim1");
+        member2.setName("kim1");
 
         //when
         memberService.join(member1);
